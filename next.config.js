971a3bf67a1ac51e40f2a826f31a66fc884845ac
Mojtaba Khodami <mojtabakh@hotmail.com>
2022-03-02 21:01:53 +0330

    updated nextjs config file for images

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ['avatars.githubusercontent.com', 'localhost'],
+		domains: ['avatars.githubusercontent.com', 'mjtbkh.github.io', 'localhost'],
+		deviceSizes: [320, 420, 768, 1024, 1200],
 		minimumCacheTTL: 2592000,
 		loader: 'imgix',
-		path: 'https://mjtbkh.imgix.net',
+		path: '', //'https://mjtbkh.imgix.net'
 	},
 };
 
